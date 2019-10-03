b4f35d52745518bb9ef0231131cee589524e2395
shrfu31 <55064372+shrfu31@users.noreply.github.com>
2019-10-03 14:19:58 -0400

    Basic HTML and folder structure

---
+++ assets/javascript/timesheet.js
@@ -0,0 +1,11 @@
+
+
+var firebaseConfig = {
+    apiKey: "AIzaSyAkGb7T0WJeezQLBcwprhQl-HneGplPCqw",
+    authDomain: "timesheet-89c97.firebaseapp.com",
+    databaseURL: "https://timesheet-89c97.firebaseio.com",
+    projectId: "timesheet-89c97",
+    storageBucket: "",
+    messagingSenderId: "455143007002",
+    appId: "1:455143007002:web:2eef0a838e7a367f9602a3"
+  };
