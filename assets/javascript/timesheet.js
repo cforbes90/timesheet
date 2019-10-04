ea5dce0345c491d07f2bac15dc21b71340edc2fd
shrfu31 <55064372+shrfu31@users.noreply.github.com>
2019-10-04 01:35:27 -0400

    Added loop logic for array of employees. Plan to change array into database reference

--- assets/javascript/timesheet.js
+++ assets/javascript/timesheet.js
@@ -8,36 +8,54 @@ var config = {
     storageBucket: "",
     messagingSenderId: "455143007002",
     appId: "1:455143007002:web:2eef0a838e7a367f9602a3"
-  };
+};
 
-  firebase.initializeApp(config);
+firebase.initializeApp(config);
 
-  // Create a variable to reference the database
-  var database = firebase.database();
+// Create a variable to reference the database
+var database = firebase.database();
 
-  // Initial Values
-  var employeeName = "";
-  var role= "";
-  var startDate = "";
-  var monthlyRate = "";
+// Initial Values
+var employeeName = "";
+var role = "";
+var startDate = "";
+var monthlyRate = 0;
+var totalEmployees =["Richard", "Cassidy", "Erik", "Nolan", "Juan"];
 
-  // Capture Button Click
-//   $("#add-user").on("click", function(event) {
-//     // Don't refresh the page!
-//     event.preventDefault();
+// Capture Button Click
+$("#add-employee").on("click", function (event) {
+    // Don't refresh the page!
+    event.preventDefault();
 
-//     // YOUR TASK!!!
-//     // Code in the logic for storing and retrieving the most recent user.
-//     // Don't forget to provide initial data to your Firebase database.
-//     name = $("#name-input").val().trim();
-//     email = $("#email-input").val().trim();
-//     age = $("#age-input").val().trim();
-//     comment = $("#comment-input").val().trim();
+    //      YOUR TASK!!!
+    //      Code in the logic for storing and retrieving the most recent user.
+    //      Don't forget to provide initial data to your Firebase database.
+    employeeName = $("#employee-name-input").val().trim();
+    role = $("#role-input").val().trim();
+    startDate = $("#start-date-input").val().trim();
+    monthlyRate = $("#monthly-rate-input").val().trim();
 
-//     database.ref().set({
-//       name: name,
-//       email: email,
-//       age: age,
-//       comment: comment
-//     });
-//   });
+    database.ref().set({
+        name: employeeName,
+        role: role,
+        startDate: startDate,
+        monthlyRate: monthlyRate
+    })
+    for (i = 0; i <= totalEmployees.length; i++) {
+        var newEmployeeRow = $("<tr>");
+        newEmployeeRow.attr("id", totalEmployees[i]);
+        $("#new-employee-row").append(newEmployeeRow)
+        //newEmployeeRow.attr("class", i)
+
+        for (a = 1; a <=6 ; a++) {
+            var newEmployee = $("<td>");
+            newEmployee.addClass("column " + a);
+            $("#" + totalEmployees[i]).append(newEmployee);
+        }
+
+
+        //$("#new-employee-row".append(newEmployee));
+
+
+    }
+});
